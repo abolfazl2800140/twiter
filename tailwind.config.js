6df4537eb79b6cade3104e8b204cd1a2260bf292
Abolfazl <abolfazl.2800140@gmail.com>
2024-12-14 22:38:21 +0330

    add-tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // این خط مسیر فایل‌های TypeScript و JSX را مشخص می‌کند
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
